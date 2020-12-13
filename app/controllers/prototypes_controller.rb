254012f68f6c2030565168b6d0c16a4231eb6729
mayuko-okamoto <astrology0928@icloud.com>
2020-12-13 18:01:04 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,56 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :new, :show]
+
+
+  def index
+    @prototypes = Prototype.all
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create 
+    if Prototype.create(prototype_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.new
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    if prototype.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+end
