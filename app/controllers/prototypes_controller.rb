8b4340fbd6b1a43769f70f6aecd507c34abc8ca5
mayuko-okamoto <astrology0928@icloud.com>
2020-12-26 19:34:43 +0900

    答え合わせ4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,16 +1,19 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :show]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
   end
-  
+
   def new
     @prototype = Prototype.new
   end
 
-  def create 
-    if @prototype = Prototype.create(prototype_params)
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
       redirect_to root_path
     else
       render :new
@@ -19,19 +22,14 @@ class PrototypesController < ApplicationController
 
   def show
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && @prototype.user_id == current_user.id
-      redirect_to action: :index
-    end
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
       render :edit
@@ -39,8 +37,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    if prototype.destroy
+    if @prototype.destroy
       redirect_to root_path
     else
       redirect_to root_path
@@ -48,8 +45,16 @@ class PrototypesController < ApplicationController
   end
 
   private
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
